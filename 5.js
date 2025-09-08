b35185733ce87aec9bbcef935fa9a40806c1e86f
MayoralAJ <ana.mayoral.1bmangal@gmail.com>
2025-09-08 08:19:08 -0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -7,4 +7,7 @@ console.log("Contagem com do...while:");
 do {
  console.log(numero); // mostra o valor
  numero++; // incrementa, ou seja, 2, até chegar em 5, linha seguinte
+
 } while (numero <= 5); // verifica a condição
+
+//a diferença entre o while e do-while é que o do-while executa o código primeiro e depois verifica a condição, já o while, verifica a condição antes de executar
